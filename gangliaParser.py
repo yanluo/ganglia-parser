2af6269b292ee9431da82719ac81e2af2fcbb859
peilong <long880420@gmail.com>
2014-01-10 17:54:56 -0500

    ganglia parser v0.1.1
    Not perfect yet

--- gangliaParser.py
+++ gangliaParser.py
@@ -1,15 +1,30 @@
+#!/usr/bin/python
+
+__author__ = 'Peilong'
+
 import urllib2
 import re
 import sys
 import json
 from pprint import pprint
 import matplotlib.pyplot as plt
+import ConfigParser
+import string
+
 
 if len (sys.argv) < 2:
-    print 'Usage: python grepip.py [Ganglia Web UI URL]'
+    print 'Usage: python gangliaParser.py [target.conf]'
     sys.exit(2)
 else:
-    gangliaUrl = sys.argv[-1]
+    filename = sys.argv[-1]
+
+
+cf = ConfigParser.ConfigParser()    
+cf.read(filename)
+
+dnsAddress = cf.get("target","dns")
+gangliaUrl = dnsAddress + ':5080/ganglia/'
+#print gangliaUrl
 
 contents = urllib2.urlopen(gangliaUrl).read()
 
@@ -22,15 +37,16 @@ for item in result:
     if item not in seen:
         seen.add(item)
         remove_duplicate.append(item)
-print remove_duplicate
+#print remove_duplicate
 
 jsonUrl = []
 for index, value in enumerate (remove_duplicate):
-    jsonUrl.append (gangliaUrl + 'graph.php?r=hour&h='+ value + '&m=load_one&s=by+namei&m=cpu_nice&mc=2&g=cpu_report&c=spark&json=1')
+    jsonUrl.append (gangliaUrl + 'graph.php?r=hour&c=spark&h='+ value + '&v=0.0&m='+cf.get("option","metric")+'&jr=&js=&vl=KB&json=1')
+#print jsonUrl
 
 jsonContents = []
 data = []
-time = []
+xaxis = []
 yaxis = []
 for index, value in enumerate (remove_duplicate):
     jsonContents.append( urllib2.urlopen(jsonUrl[index]).read() )
@@ -38,11 +54,26 @@ for index, value in enumerate (remove_duplicate):
     localFile.write(jsonContents[index])
     localFile.close()
 
+    xaxistmp = []
+    yaxistmp = []
+
     jsontmp = json.loads(jsonContents[index])
     data.append(jsontmp)
-    for i in xrange (0, len(data[index][0]['datapoints'])):
-        time.append(data[index][0]['datapoints'][i][1])
-        yaxis.append(data[index][0]['datapoints'][i][0])
+    if data[index] is not None:
+        for i in xrange (0, len(data[index][0]['datapoints'])):
+            xaxistmp.append(data[index][0]['datapoints'][i][1])
+            yaxistmp.append(data[index][0]['datapoints'][i][0])
+        xaxis.append(xaxistmp)
+        yaxis.append(yaxistmp)
 
-plt.plot(time,yaxis, 'k')
+print '-'*60
+
+for index, value in enumerate (xaxis):
+    plt.figure(index)
+    plt.title(remove_duplicate[index])
+    plt.plot(xaxis[index], yaxis[index], 'k')
+    plt.ylabel('Value')
+    plt.xlabel('Time')
+    
 plt.show()
+    
