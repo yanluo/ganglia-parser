2d50f5155e38e5a70561b78d5d3968591fd97252
peilong <long880420@gmail.com>
2014-01-10 22:44:54 -0500

    Ganglia Parser v0.1.4
    README, Comments on code

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ ganglia-parser
 ==============
 
 Parser to retrieve and plot Ganglia metrics from an EC2 cluster
+
+--------------
+How to use:
+
+1. Configure the "target.conf" file with the parameters required 
+in each section. Here is a simple example:
+	[target]
+	dns=http://ec2-54-221-160-93.compute-1.amazonaws.com
+
+	[option]
+	metric=cpu_system
+
+	[node]
+	number=2
+This file contains the DNS server where Ganglia gmetad process
+is running, the metric option and the number of nodes.
+
+2. Run script: ./gangliaParser.py target.conf
+or python gangliaParser.py target.conf
+
+3. Two kinds of files will be automatically saved to the current
+directry. One is JSON files that contains the wanted metric data,
+the other is png figures that visualized the JSON data.
--- gangliaParser.py
+++ gangliaParser.py
@@ -19,21 +19,28 @@ if len (sys.argv) < 2:
 else:
     filename = sys.argv[-1]
 
+### Header
 print '-'*45
-print ' *** Ganglia Visualization Tool V-0.1.3 ***'
+print ' *** Ganglia Visualization Tool V-0.1.4 ***'
 print ''
 print ' Usage: python gangliaParser.py target.conf'
 print '-'*45
 
+### Configuration file parser
 cf = ConfigParser.ConfigParser()    
 cf.read(filename)
 
 dnsAddress = cf.get("target","dns")
-gangliaUrl = dnsAddress + ':5080/ganglia/'
-#print gangliaUrl
+metric = cf.get("option","metric")
+node_number = int (cf.get("node","number"))
 
+### Ganglia web UI IP address
+gangliaUrl = dnsAddress + ':5080/ganglia/'
+
+### Ganglia web UI index page
 contents = urllib2.urlopen(gangliaUrl).read()
 
+### Regular experssion matching to grab the node internal IP
 pattern = re.compile('ip-\w{1,3}-\w{1,3}-\w{1,3}-\w{1,3}\.ec2\.internal')
 result = pattern.findall(contents)
 
@@ -43,20 +50,22 @@ for item in result:
     if item not in seen:
         seen.add(item)
         remove_duplicate.append(item)
-#print remove_duplicate
 
+### Concatenate the final URL where JSON data locates
 jsonUrl = []
 for index, value in enumerate (remove_duplicate):
-    jsonUrl.append (gangliaUrl + 'graph.php?r=hour&c=spark&h='+ value + '&v=0.0&m='+cf.get("option","metric")+'&jr=&js=&vl=KB&json=1')
+    jsonUrl.append (gangliaUrl + 'graph.php?r=hour&c=spark&h='+ value + '&v=0.0&m='+ metric +'&jr=&js=&vl=KB&json=1')
 #print jsonUrl
 
 jsonContents = []
 data = []
 xaxis = []
 yaxis = []
-for index, value in enumerate (remove_duplicate):
+
+### Generate the JSON files
+for index in xrange (0, node_number):
     jsonContents.append( urllib2.urlopen(jsonUrl[index]).read() )
-    localFile = open( value+'.json', 'w' )
+    localFile = open( remove_duplicate[index] + '-' + metric +'.json', 'w' )
     localFile.write(jsonContents[index])
     localFile.close()
 
@@ -72,14 +81,16 @@ for index, value in enumerate (remove_duplicate):
         xaxis.append(xaxistmp)
         yaxis.append(yaxistmp)
 
-
-graph_number = int (cf.get("node","number"))
-for index in xrange (0,graph_number):
+### Plot JSON data to png figures
+for index in xrange (0, node_number):
     plt.figure(index)
     plt.title(remove_duplicate[index])
     plt.plot(xaxis[index], yaxis[index], 'k')
-    plt.ylabel('Value')
+    plt.ylabel('Value '+ metric)
     plt.xlabel('Time (1 hour in total)')
-    plt.savefig(remove_duplicate[index]+'.png')
+    plt.savefig(remove_duplicate[index] + '-' + metric +'.png')
 
-    
+### Final printout message
+print '+'*45
+print '[SUCCESS] .JSON and .PNG files are saved!'
+print '+'*45
