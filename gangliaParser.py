6e55c828007031ee6daf05ec0b9ea1a4bbad46b2
Yan Luo <yanluo.uml@gmail.com>
2014-01-11 11:55:15 -0500

    restructure code and conf file

--- gangliaParser.py
+++ gangliaParser.py
@@ -12,6 +12,44 @@ import ConfigParser
 import string
 import decimal
 
+def get_metric(remove_duplicate, metric):
+    for index, value in enumerate (remove_duplicate):
+        #print index, value
+        ### Concatenate the final URL where JSON data locates
+        jsonUrl = gangliaUrl + 'graph.php?r=hour&c=spark&h='+ value + '&v=0.0&m='+ metric +'&jr=&js=&json=1'
+        print "--> from host: ", value 
+
+        ### Generate the JSON files
+        response = urllib2.urlopen(jsonUrl)
+        jsondata = json.load(response)
+        localFile = open( remove_duplicate[index] + '-' + metric +'.json', 'w' )
+        #print "data =", jsondata
+        json.dump(jsondata, localFile)
+        localFile.close()
+
+        # check if json file is empty or contains "null"
+        if jsondata == None:
+            continue
+
+        xaxis = []
+        yaxis = []
+
+        #print jsondata[0]
+        rawdata = jsondata[0]['datapoints']
+        #print "rawdata=", rawdata
+        for i in xrange (0, len(rawdata)):
+            xaxis.append(rawdata[i][1])
+            yaxis.append(rawdata[i][0])
+
+        ### Plot JSON data to png figures
+        plt.figure(index)
+        plt.title(remove_duplicate[index])
+        plt.plot(xaxis[index], yaxis[index], 'k')
+        plt.ylabel('Value '+ metric)
+        plt.xlabel('Time (1 hour in total)')
+        plt.savefig(remove_duplicate[index] + '-' + metric +'.png')
+
+    return
 
 if len (sys.argv) < 2:
     print 'Usage: python gangliaParser.py target.conf'
@@ -21,7 +59,7 @@ else:
 
 ### Header
 print '-'*45
-print ' *** Ganglia Visualization Tool V-0.1.4 ***'
+print ' *** Ganglia Visualization Tool V-0.2 ***'
 print ''
 print ' Usage: python gangliaParser.py target.conf'
 print '-'*45
@@ -31,11 +69,12 @@ cf = ConfigParser.ConfigParser()
 cf.read(filename)
 
 dnsAddress = cf.get("target","dns")
-metric = cf.get("option","metric")
-node_number = int (cf.get("node","number"))
+# metrics is comma separated list
+metrics = cf.get("option","metrics")
+#node_number = int (cf.get("node","number"))
 
 ### Ganglia web UI IP address
-gangliaUrl = dnsAddress + ':5080/ganglia/'
+gangliaUrl = 'http://'+dnsAddress + ':5080/ganglia/'
 
 ### Ganglia web UI index page
 contents = urllib2.urlopen(gangliaUrl).read()
@@ -51,44 +90,13 @@ for item in result:
         seen.add(item)
         remove_duplicate.append(item)
 
-### Concatenate the final URL where JSON data locates
-jsonUrl = []
-for index, value in enumerate (remove_duplicate):
-    jsonUrl.append (gangliaUrl + 'graph.php?r=hour&c=spark&h='+ value + '&v=0.0&m='+ metric +'&jr=&js=&vl=KB&json=1')
-#print jsonUrl
+#print "set size =", len(seen)
+num_nodes = len(seen)
 
-jsonContents = []
-data = []
-xaxis = []
-yaxis = []
-
-### Generate the JSON files
-for index in xrange (0, node_number):
-    jsonContents.append( urllib2.urlopen(jsonUrl[index]).read() )
-    localFile = open( remove_duplicate[index] + '-' + metric +'.json', 'w' )
-    localFile.write(jsonContents[index])
-    localFile.close()
-
-    xaxistmp = []
-    yaxistmp = []
-
-    jsontmp = json.loads(jsonContents[index])
-    data.append(jsontmp)
-    if data[index] is not None:
-        for i in xrange (0, len(data[index][0]['datapoints'])):
-            xaxistmp.append(data[index][0]['datapoints'][i][1])
-            yaxistmp.append(data[index][0]['datapoints'][i][0])
-        xaxis.append(xaxistmp)
-        yaxis.append(yaxistmp)
-
-### Plot JSON data to png figures
-for index in xrange (0, node_number):
-    plt.figure(index)
-    plt.title(remove_duplicate[index])
-    plt.plot(xaxis[index], yaxis[index], 'k')
-    plt.ylabel('Value '+ metric)
-    plt.xlabel('Time (1 hour in total)')
-    plt.savefig(remove_duplicate[index] + '-' + metric +'.png')
+# get all the metrics one by one
+for metric in metrics.split(','):
+    print "Getting metric "+metric
+    get_metric(remove_duplicate, metric)
 
 ### Final printout message
 print '+'*45
